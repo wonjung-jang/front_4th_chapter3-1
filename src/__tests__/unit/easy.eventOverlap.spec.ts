9a7a5767ced9edb87b7b0f990162b89e541e6ef2
장원정 <jang_expedition@gmail.com>
2025-02-03 22:14:20 +0900

    test: convertEventToDateRange 테스트 작성

--- src/__tests__/unit/easy.eventOverlap.spec.ts
+++ src/__tests__/unit/easy.eventOverlap.spec.ts
@@ -29,11 +29,26 @@ describe('parseDateTime', () => {
 });
 
 describe('convertEventToDateRange', () => {
-  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {});
+  it('일반적인 이벤트를 올바른 시작 및 종료 시간을 가진 객체로 변환한다', () => {
+    const event = events[0] as Event;
+    const timeObject = convertEventToDateRange(event);
+    expect(timeObject.start).toEqual(new Date(2024, 9, 15, 9, 0));
+    expect(timeObject.end).toEqual(new Date(2024, 9, 15, 10, 0));
+  });
 
-  it('잘못된 날짜 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+  it('잘못된 날짜 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {
+    const event = { date: '2024_07_01', startTime: '14:30', endTime: '15:30' } as Event;
+    const timeObject = convertEventToDateRange(event);
+    expect(timeObject.start.getTime()).toBeNaN();
+    expect(timeObject.end.getTime()).toBeNaN();
+  });
 
-  it('잘못된 시간 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {});
+  it('잘못된 시간 형식의 이벤트에 대해 Invalid Date를 반환한다', () => {
+    const event = { date: '2024-07-01', startTime: '14/30', endTime: '15/30' } as Event;
+    const timeObject = convertEventToDateRange(event);
+    expect(timeObject.start.getTime()).toBeNaN();
+    expect(timeObject.end.getTime()).toBeNaN();
+  });
 });
 
 describe('isOverlapping', () => {
