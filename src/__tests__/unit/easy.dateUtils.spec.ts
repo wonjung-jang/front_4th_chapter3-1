abb57c059c9b74c5a78d0a5355fc3666de42bbd4
장원정 <jang_expedition@gmail.com>
2025-02-03 16:14:42 +0900

    test: formatMonth 테스트 작성

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -233,7 +233,10 @@ describe('formatWeek', () => {
 });
 
 describe('formatMonth', () => {
-  it("2024년 7월 10일을 '2024년 7월'로 반환한다", () => {});
+  it("2024년 7월 10일을 '2024년 7월'로 반환한다", () => {
+    const formattedMonth = formatMonth(new Date('2024-07-10'));
+    expect(formattedMonth).toBe('2024년 7월');
+  });
 });
 
 describe('isDateInRange', () => {
