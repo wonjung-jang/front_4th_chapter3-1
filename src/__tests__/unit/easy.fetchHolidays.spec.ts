9707eaf77aa741c2622a383198ddccb98e4b4f9c
장원정 <jang_expedition@gmail.com>
2025-02-03 22:13:46 +0900

    test: fetchHolidays 테스트 작성

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,9 +1,24 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const holidays = fetchHolidays(new Date('2024-01'));
+    expect(holidays).toEqual({
+      '2024-01-01': '신정',
+    });
+  });
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const holidays = fetchHolidays(new Date('2024-04'));
+    expect(holidays).toEqual({});
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const holidays = fetchHolidays(new Date('2024-02'));
+    expect(holidays).toEqual({
+      '2024-02-09': '설날',
+      '2024-02-10': '설날',
+      '2024-02-11': '설날',
+    });
+  });
 });
