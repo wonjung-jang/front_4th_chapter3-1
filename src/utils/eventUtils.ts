3a5ea093da9e738da4f635eb533ec7eef28b8599
장원정 <jang_expedition@gmail.com>
2025-02-05 13:27:32 +0900

    refactor: filterEventsByDateRangeAtMonth 함수 시작, 끝 시간 UTC 시간으로 조회하도록 변경

--- src/utils/eventUtils.ts
+++ src/utils/eventUtils.ts
@@ -25,8 +25,8 @@ function filterEventsByDateRangeAtWeek(events: Event[], currentDate: Date) {
 }
 
 function filterEventsByDateRangeAtMonth(events: Event[], currentDate: Date) {
-  const monthStart = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-  const monthEnd = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+  const monthStart = new Date(Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), 1));
+  const monthEnd = new Date(Date.UTC(currentDate.getFullYear(), currentDate.getMonth() + 1, 0));
   return filterEventsByDateRange(events, monthStart, monthEnd);
 }
 
