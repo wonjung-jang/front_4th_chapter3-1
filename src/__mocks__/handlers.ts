5b3e3e69e751bd0705ad27cd683b2d1c58b048d8
장원정 <jang_expedition@gmail.com>
2025-02-06 18:09:06 +0900

    feat: handler 작성

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,16 +1,45 @@
 import { http, HttpResponse } from 'msw';
 
-import { Event } from '../types';
-import { events } from './response/events.json' assert { type: 'json' };
+import { Event, EventForm } from '../types';
+import eventsData from './response/events.json' assert { type: 'json' };
+
+let events = { ...eventsData };
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json(events);
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const eventFormData = (await request.json()) as EventForm;
+    const newEvent = {
+      ...eventFormData,
+      id: (events.events.length + 1).toString(),
+    } as Event;
+    events.events.push(newEvent);
+    return HttpResponse.json(newEvent, { status: 201 });
+  }),
 
-  http.put('/api/events/:id', async () => {}),
+  http.put('/api/events/:id', async ({ request, params }) => {
+    const updatedEvent = (await request.json()) as Event;
+    events.events = events.events.map((e) => (e.id === params.id ? updatedEvent : e));
+    return HttpResponse.json(updatedEvent, { status: 200 });
+  }),
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+  http.delete('/api/events/:id', ({ params }) => {
+    events.events = events.events.filter((e) => e.id !== params.id);
+    return HttpResponse.json({ message: 'Event deleted' }, { status: 200 });
+  }),
 ];
+
+export const setupHandlers = (initEvents = [] as Event[]) => {
+  if (initEvents.length) {
+    events.events = [...initEvents];
+  } else {
+    events.events = [...eventsData.events];
+  }
+
+  return handlers;
+};
